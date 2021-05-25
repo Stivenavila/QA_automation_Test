7fb693732157d18cc9b76cff0dd4c3167b888b8a
Clist <def get_number(self):         return self.driver.find_element(*self.Number_mee).get_attribute("value")>
2021-05-25 13:10:11 -0500

    Creacion de Paquete inicial que contendra las ejecuciones de la automatizacion

---
+++ pageobject/base_page.py
@@ -0,0 +1,10 @@
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
+
+    '''El método find_element(self, *locator) tomará como argumento el localizador que le especifiquemos'''
+
+
